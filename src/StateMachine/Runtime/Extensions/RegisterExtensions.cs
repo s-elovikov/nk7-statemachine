5402dbdca1be09d253bb76470703a0a1b3465105
lsd7nk <87329383+lsd7nk@users.noreply.github.com>
2025-10-24 23:17:25 +0700

    remove nk7-container from package dependencies

--- src/StateMachine/Runtime/Extensions/RegisterExtensions.cs
+++ src/StateMachine/Runtime/Extensions/RegisterExtensions.cs
@@ -1,15 +1,20 @@
-using Nk7.Container;
 using System;
+
+#if NK7_CONTAINER
+using Nk7.Container;
+#endif
 
 namespace Nk7.StateMachine
 {
     public static class RegisterExtensions
     {
+#if NK7_CONTAINER
         public static void RegisterStateMachine<TTrigger>(this IBaseDIService diService, IDIContainer container)
             where TTrigger : Enum
         {
             diService.RegisterFactory<IState<TTrigger>>(container);
             diService.RegisterTransient<IStateMachine<TTrigger>, StateMachine<TTrigger>>();
         }
+#endif
     }
 }
